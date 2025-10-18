deed91662d8e244c10c0d37f94ecd33b5ab7446e
fitriayuniar <fitriafauziah09@gmail.com>
2025-10-18 10:33:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const articles = [
+  {
+    source: { name: 'Sawit Indonesia' },
+    author: 'Admin',
+    title: 'Menteri Pertanian Menindaklanjuti Instruksi Presiden untuk Revitalisasi Pabrik Pupuk',
+    description: 'Menteri Pertanian Andi Amran Sulaiman menegaskan pihaknya siap menindaklanjuti Instruksi Presiden Prabowo Subianto untuk revitalisasi pabrik pupuk guna mendukung ketahanan pangan nasional.',
+    url: 'https://sawitindonesia.com/menteri-pertanian-menindaklanjuti-instruksi-presiden-untuk-revitalisasi-pabrik-pupuk/',
+    urlToImage: 'https://sawitindonesia.com/wp-content/uploads/2025/10/menteri-pertanian.jpg',
+    publishedAt: '2025-10-17T10:00:00Z'
+  },
+  {
+    source: { name: 'Antara News' },
+    author: 'Siti',
+    title: 'XPeng Indonesia Resmi Hadirkan Diler Flagship Terbaru di Pondok Indah',
+    description: 'XPeng Indonesia secara resmi menghadirkan diler flagship terbarunya yang terletak di kawasan elit Pondok Indah, Jakarta Selatan, untuk semakin mendekatkan diri dengan konsumen.',
+    url: 'https://www.antaranews.com/berita/5180009/xpeng-indonesia-resmi-hadirkan-diler-flagship-terbaru-di-pondok-indah',
+    urlToImage: 'https://www.antaranews.com/images/2025/10/17/xpeng-diler-pondok-indah.jpg',
+    publishedAt: '2025-10-17T09:30:00Z'
+  },
+  {
+    source: { name: 'Antara News' },
+    author: 'Budi',
+    title: 'Bedah Spek Seri iPhone 17 yang Kini Resmi di Indonesia',
+    description: 'Seri ponsel iPhone 17 yang terdiri atas iPhone 17, iPhone 17 Pro, dan iPhone 17 Pro Max akhirnya secara resmi telah dijual di Indonesia dengan berbagai fitur canggih.',
+    url: 'https://jogja.antaranews.com/berita/776529/bedah-spek-seri-iphone-17-yang-kini-resmi-di-indonesia',
+    urlToImage: 'https://jogja.antaranews.com/images/2025/10/17/iphone-17-indonesia.jpg',
+    publishedAt: '2025-10-17T08:45:00Z'
+  },
+  {
+    source: { name: 'Bola.com' },
+    author: 'TechToday',
+    title: 'Peringkat FIFA Terbaru Tim-Tim ASEAN: Thailand Kembali ke 100 Besar, Indonesia Turun',
+    description: 'Timnas Thailand menempati posisi ke-96 dalam sistem peringkat FIFA, sementara Indonesia mengalami penurunan peringkat.',
+    url: 'https://www.bola.com/indonesia/read/6188290/peringkat-fifa-terbaru-tim-tim-asean-thailand-kembali-ke-100-besar-indonesia-turun',
+    urlToImage: 'https://www.bola.com/images/2025/10/17/fifa-asean.jpg',
+    publishedAt: '2'
+  }
+]
